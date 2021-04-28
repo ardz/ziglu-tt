289930949735b225d5f3b69762c2b68b945e9718
dickyp <richard.peckett@outlook.com>
2021-04-28 17:38:22 +0100

    name/id

--- test/ZigluFeatures/Features/CoinComparisons.cs
+++ test/ZigluFeatures/Features/CoinComparisons.cs
@@ -25,39 +25,39 @@ namespace ZigluFeatures.Features
         /// <summary>
         /// BDD wouldn't be at this level in the real world :S
         /// </summary>
-        /// <param name="coinName"></param>
+        /// <param name="coinId"></param>
         [Scenario]
         [InlineData("telcoin")]
         //[InlineData("xtz")] I can't see a coin with this id
         [InlineData("nano")]
-        public void UserCanQueryCoinValueInGbp(string coinName)
+        public void UserCanQueryCoinValueInGbp(string coinId)
         {
             Coin coin = null;
 
-            $"When the user queries coin data for {coinName}"
+            $"When the user queries coin data for {coinId}"
                 .x(async () =>
                 {
-                    coin = await ZigluService.CoinInfo(coinName);
+                    coin = await ZigluService.CoinInfo(coinId);
                 });
 
-            $"Then the user sees data for {coinName}"
+            $"Then the user sees data for {coinId}"
                 .x(() =>
                 {
                     _output.WriteLine(Service.ModelToTextOutput(coin));
                     
-                    Assert.Equal(coinName, coin.Name, true);
+                    Assert.Equal(coinId, coin.Name, true);
                 });
 
-            $"And the user sees the value of {coinName} in GBP"
+            $"And the user sees the value of {coinId} in GBP"
                 .x(() => { coin.MarketValue.Should().NotBe(default); });
 
-            $"And the market cap for {coinName}"
+            $"And the market cap for {coinId}"
                 .x(() => { coin.MarketCap.Should().NotBe(default); });
 
-            $"And the 24 hour GBP price change for {coinName}"
+            $"And the 24 hour GBP price change for {coinId}"
                 .x(() => { coin.PriceChange.Should().NotBe(default); });
 
-            $"And the time the coin data was last updated {coinName}"
+            $"And the time the coin data was last updated {coinId}"
                 .x(() => { coin.LastUpdated.Should().NotBe(default); });
         }
     }
